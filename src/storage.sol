5864d074bf0d18ea035809a5cb9b576fdc5fab2e
chanaKastner <133571828+chanaKastner@users.noreply.github.com>
2024-03-13 19:44:18 +0200

    Update storage.sol

--- src/storage.sol
+++ src/storage.sol
@@ -1,4 +1,3 @@
-
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.7.0 <0.9.0;
 
@@ -6,18 +5,21 @@ pragma solidity >=0.7.0 <0.9.0;
 /// @dev Store & retrieve value in a variable
 
 contract Storage {
+  
     uint256 number;
 
     /// @dev Store value in variable
     /// @param num value to store
 
-    function store(uint256 num) public {
+    function store (uint256 num) public {
+       // storing the number we got in to number
         number = num;
     }
 
     /// @dev Return value
     /// @return value of 'number'
 
+    // what doew view mean?
     function retrieve() public view returns (uint256){
         return number;
     }
