82ff55e2ce2cfc9ff4eacb935f5483ba18530d63
chanaKastner <133571828+chanaKastner@users.noreply.github.com>
2024-03-18 16:53:20 +0200

    Create BeitHaknesset.sol

---
+++ src/BeitHaknesset.sol
@@ -0,0 +1,77 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+/// @title Wallet
+/// @author Chana Kastner
+
+contract BeitHaknesset {
+    address public owner;
+
+    mapping(address => bool) public gabaim;
+
+    uint256 public gabaimCount;
+
+    uint256 constant MAX_GABAIM = 3;
+
+    constructor() {
+        owner = msg.sender;
+        gabaim[msg.sender] = true;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only the owner can call this function");
+        _;
+    }
+
+    /// @dev a function that can recieve money that was deposited to my wallet
+
+    receive() external payable {}
+
+    /// @dev Withdraw money from the wallet
+    /// @param withdrawAmount - the amount of ETH to withdraw
+
+    function withdraw(uint256 withdrawAmount) public onlyOwner {
+        require(gabaim[msg.sender], "Only the owner & the gabaim can withdraw");
+
+        require(
+            address(this).balance >= withdrawAmount,
+            "There is not enough Eth to withdraw"
+        );
+
+        payable(owner).transfer(withdrawAmount);
+
+    }
+
+    /// @dev View the wallet's balance
+
+    function getBalance() public view returns (uint256) {
+        return address(this).balance;
+    }
+
+    /// @dev add gabaim to the mapping
+    /// @param gabai - the gabai i want to add
+
+    function addGabaim(address gabai) public onlyOwner {
+        require(gabaim[gabai] == false, "Gabai already exists");
+        require(gabaimCount < MAX_GABAIM, "Maximum gabaim reached");
+        gabaim[gabai] = true;
+        gabaimCount++;
+    }
+
+    /// @dev delete gabai
+    /// @param gabai - the gabai i want to delete
+
+    function deleteGabai(address gabai) public onlyOwner {
+        require(gabaim[gabai] == true, "Gabai does not exist");
+        delete gabaim[gabai];
+        gabaimCount--;
+    }
+
+    /// @dev change gabai
+    /// @param newgabai - the gabai i want to add, oldGabai - the gabai i want to delete
+
+    function changeGabai(address newgabai, address oldGabai) public onlyOwner {
+        addGabaim(newgabai);
+        deleteGabai(oldGabai);
+    }
+}
