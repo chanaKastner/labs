83a77cfd5d2be4ae35f77f5612aff80fef134dc5
chanaKastner <133571828+chanaKastner@users.noreply.github.com>
2024-03-18 09:59:32 +0200

    Create Wallet1.sol

---
+++ src/Wallet1.sol
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+/// @title Wallet
+/// @author Chana Kastner
+
+contract Wallet {
+      
+    address public owner;
+
+    mapping (address=> bool)  public gabaim;
+
+    constructor() {
+        owner = msg.sender;    
+    }
+
+    /// @dev a function that can recieve money that was deposited to my wallet
+
+    receive() external payable { }
+
+    /// @dev Withdraw money from the wallet
+    /// @param withdrawAmount - the amount of ETH to withdraw
+
+    function withdraw(uint256 withdrawAmount) public {
+       
+        require(gabaim[msg.sender], "WALLET-not-owner");
+
+        require(address(this).balance >= withdrawAmount, "There is not enough Eth to withdraw");
+
+        payable(owner).transfer(withdrawAmount);
+
+        // The wallet balance is updated automatically
+    }
+
+    /// @dev View the wallet's balance
+
+    function getBalance() public view returns (uint256) {
+        return address(this).balance;
+    }
+
+    /// @dev add gabaim to the mapping
+
+    function addGabaim(address gabai) public {
+        require(owner == msg.sender, " ");
+        gabaim[gabai] = true;
+    }
+}
